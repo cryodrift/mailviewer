5f78b86108e3fb7e6d1a6663058df447b786e3b4
ego <ego@nomail.lan>
2026-01-16 14:05:40 +0100

    rem src

--- src/mailviewer/db/d_views.sql
+++
@@ -1,4 +0,0 @@
-SELECT 'DROP VIEW IF EXISTS "' || name || '";'
-FROM sqlite_master
-WHERE type = 'view';
---END;
--- src/mailviewer/db/s_content.sql
+++
@@ -1,5 +0,0 @@
-select mail_content.*
-from mail_content,
-     mail
-where mail.id = :id
-  and mail_content.id = mail.mail_content_id;
